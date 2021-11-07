04138a47121ca525b4277fb5c011d9d7c6bb3941
Dawid Kiciński <kicinski.dawid@gmail.com>
2021-11-07 19:15:26 +0100

    Initialize repository

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "pffft/pffft.h"
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    std::cout << "Hello, world" << std::endl;
+     
+    return 0;
+}
